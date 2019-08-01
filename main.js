69a7e35b0072f131bae03cd37a45b9ddf3f3f784
DariusEdwards <52717004+DariusEdwards@users.noreply.github.com>
2019-08-01 14:03:50 -0400

    Add files via upload

---
+++ main.js
@@ -0,0 +1,15 @@
+// let userData = [
+//     userData1 = 
+
+// ]
+
+
+
+function rand() {
+    let x = Math.random();
+    let times10 = x * 11;
+    let final = Math.floor(times10);
+    console.log(final);
+ }
+ rand();
+
